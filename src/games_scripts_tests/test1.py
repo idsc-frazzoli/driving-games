d4eab1c5b082ecf56a616589959d34ed086f2c76
Andrea Censi <acensi@ethz.ch>
2020-04-28 20:47:15 +0200

    fix

--- src/games_scripts_tests/test1.py
+++ src/games_scripts_tests/test1.py
@@ -11,7 +11,10 @@ from games_scripts.demo import without_compmake
 
 def test_run1():
     do_games = ["game1"]
-    do_solvers = ["solver1"]
+    do_solvers = ["solver-1-strategy-security",
+                  "solver-1-strategy-mix",
+                  ]
+
     games = {k: games_zoo[k] for k in do_games}
     solvers = {k: solvers_zoo[k] for k in do_solvers}
     res = without_compmake(games, solvers)
