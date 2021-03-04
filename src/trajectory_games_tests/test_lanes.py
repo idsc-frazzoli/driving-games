498f04c407291057c0c1045c40c28c620ec7ae5e
sirishamz <sirishs@ethz.ch>
2021-03-04 19:56:39 +0100

    Added all lanes, viz for lanes test

--- src/trajectory_games_tests/test_lanes.py
+++ src/trajectory_games_tests/test_lanes.py
@@ -1,10 +1,16 @@
 import os
+from os.path import join
+from typing import Dict
+
+from reprep import Report
 from yaml import safe_load
 import numpy as np
 import geometry as geo
+from decimal import Decimal as D
 
-from trajectory_games import config_dir
-from world import load_driving_game_map, get_lane_from_node_sequence
+from games import PlayerName
+from trajectory_games import config_dir, TrajGameVisualization, TrajectoryWorld, VehicleGeometry
+from world import load_driving_game_map, get_lane_from_node_sequence, Lane
 
 
 def test_lanes():
@@ -16,17 +22,48 @@ def test_lanes():
 
     duckie_map = load_driving_game_map(map_name)
 
+    lanes: Dict[PlayerName, Lane] = {}
+    geometries: Dict[PlayerName, VehicleGeometry] = {}
+
+    def wrap(ang: float) -> float:
+        while ang > +np.pi: ang -= 2*np.pi
+        while ang < -np.pi: ang += 2*np.pi
+        return abs(ang)
+
+    i = 1
     for k, l in config_lanes.items():
         lane = get_lane_from_node_sequence(m=duckie_map, node_sequence=l)
+        good = True
         print(f"\nLane = {k}")
-        for along1 in np.linspace(0.0, lane.get_lane_length(), 100):
+        for along1 in np.linspace(0.0, lane.get_lane_length(), 500):
             beta1 = lane.beta_from_along_lane(along1)
             point1 = lane.center_point(beta1)
             p1, r1, _ = geo.translation_angle_scale_from_E2(point1)
             beta2, point2 = lane.find_along_lane_closest_point(p1)
             along2 = lane.along_lane_from_beta(beta2)
             p2, r2, _ = geo.translation_angle_scale_from_E2(point2)
-            if abs(beta1 - beta2) > 1e-1 or abs(along1 - along2) > 1e-1 or abs(r1-r2) > 1e-1:
+            if abs(beta1 - beta2) > 0.1 or abs(along1 - along2) > 0.5 or wrap(r1-r2) > 0.1:
                 print(f"Warning, points aren't equal: \n\tbeta = {beta1, beta2} "
                       f"\n\talong = {along1, along2} \n\tangle = {r1, r2}")
+                good = False
                 break
+        if good:
+            print("Lane is good, no discontinuities")
+        else:
+            player = PlayerName(f"P_{i}")
+            lanes[player] = lane
+            geometries[player] = VehicleGeometry(m=D("100"), w=D("1"), l=D("1"), colour=(1, 0, 0))
+            i += 1
+
+    print(f"\n\nTotal bad lanes = {i-1}")
+    world = TrajectoryWorld(map_name=map_name, geo=geometries, lanes=lanes)
+    viz = TrajGameVisualization(world=world)
+
+    r = Report("Trajectories")
+    with r.plot("actions") as pylab:
+        ax = pylab.gca()
+        with viz.plot_arena(pylab, ax):
+            pass
+    d = "out/tests/"
+    r.to_html(join(d, "r_lanes.html"))
+
