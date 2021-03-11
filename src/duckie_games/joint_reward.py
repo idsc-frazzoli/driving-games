85aef7841b7f1f8abb0fe7999c2bfbd9a3297719
Christoph Ungricht <christoph.ungricht@outlook.com>
2021-03-11 14:34:44 +0100

    Updated joint reward be be used with lru cache

--- src/duckie_games/joint_reward.py
+++ src/duckie_games/joint_reward.py
@@ -1,11 +1,16 @@
 from typing import FrozenSet, Mapping
 
+from frozendict import frozendict
+
+from functools import lru_cache
 from duckietown_world.utils import memoized_reset
 
 from games import JointRewardStructure, PlayerName
 from driving_games.collisions import Collision
 
 from duckie_games.collisions_check import (
+    spatial_collision_check_binary_resources_no_rectangles,
+    spatial_collision_check_binary_resources_no_rectangles_players_only,
     spatial_collision_check_resources_no_energy_players_only,
     spatial_collision_check_resources_no_energy,
     collision_check_resources_no_energy,
@@ -27,14 +32,24 @@ class DuckieJointReward(JointRewardStructure[DuckieState, DuckieActions, Collisi
         self.geometries = geometries
         self.dynamics = dynamics
 
-    # @lru_cache(None)
-    @memoized_reset
+    # todo find suitable collision function
+    # @memoized_reset
+    @lru_cache(None)
     def is_joint_final_state(self, xs: Mapping[PlayerName, DuckieState]) -> FrozenSet[PlayerName]:
         res = spatial_collision_check_resources_no_energy_players_only(xs, self.geometries, self.dynamics)
+
+        # only for debugging
+        # res = spatial_collision_check_binary_resources_no_rectangles_players_only(xs, self.geometries, self.dynamics)
         return frozenset(res)
 
-    # @lru_cache(None)
-    @memoized_reset
+    # @memoized_reset
+    @lru_cache(None)
     def joint_reward(self, xs: Mapping[PlayerName, DuckieState]) -> Mapping[PlayerName, Collision]:
         res = spatial_collision_check_resources_no_energy(xs, self.geometries, self.dynamics)
+
+        # only for debugging
+        # res = spatial_collision_check_binary_resources_no_rectangles(xs, self.geometries, self.dynamics)
         return res
+
+    def __hash__(self):  # to be able to use lru cache
+        return hash(frozendict(self.__dict__))
