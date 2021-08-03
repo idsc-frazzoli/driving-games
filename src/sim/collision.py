Add relative velocity along normal of impact calculation (fix!) - Currently the normal of impact corresponds to the vector between center of the obj and the closest point on the other object, which might not be correct

src/sim/collision.py
@@ -5,7 +5,7 @@ from commonroad_dc.pycrcc import RectOBB
 
 from games import PlayerName
 from sim import ImpactLocation, CollisionReport
-from sim.collision_utils import get_rectangle_mesh
+from sim.collision_utils import get_rectangle_mesh, get_normal_of_impact
 from sim.simulator import SimContext
 
 
@@ -49,7 +49,9 @@ def compute_collision_report(a: PlayerName, b: PlayerName, sim_context: SimConte
     # rel_velocity = np.array([0, 0])
     rel_velocity = get_relative_velocity(a_vel, b_vel)
     # todo relative velocity along normal
-    rel_velocity_along_n = 0
+    n = get_normal_of_impact(a_shape, b_shape)
+    rel_velocity_along_n = np.dot(rel_velocity, n)
+    rel_velocity_along_n = np.linalg.norm(rel_velocity_along_n)
     # todo energy transferred
     energy = 0
     return CollisionReport(location=locations,

src/sim/collision_structures.py
@@ -27,9 +27,9 @@ class CollisionReport:
     at_fault: bool
     """ At fault is defined as...."""
     rel_velocity: np.ndarray
-    """ Relative velocity defined as v_b-v_a in global RF"""
+    """ Relative velocity defined as v_a-v_b in global RF"""
     rel_velocity_along_n: float
-    """ Relative velocity defined as dot(v_b,n)-dot(v_a,n), with n the normal of impact"""
+    """ Relative velocity defined as dot(v_a,n)-dot(v_b,n), with n the normal of impact"""
     energy_transfer: float
     """ """
     at_time: float #SimTime

src/sim/collision_utils.py
@@ -1,22 +1,23 @@
-from typing import Mapping, List
+from typing import Mapping, List, Tuple, Any
 
 import numpy as np
 from commonroad_dc import pycrcc
 from geometry import SE2_from_xytheta, SE2value
+from shapely.geometry import Point, Polygon
+from shapely.ops import nearest_points
 
 from sim import ImpactLocation, IMPACT_FRONT, IMPACT_BACK, IMPACT_LEFT, IMPACT_RIGHT
 from sim.models.vehicle import VehicleState
 
 
-def get_vertices_from_rectangle(rect: pycrcc.RectOBB) -> List[List[float]]:
-    """ This gets the car vertices in the global reference frame (RF)
+def get_vertices_as_list(rect: pycrcc.RectOBB) -> List[List[float]]:
+    """ This gets the car vertices in the global reference frame (RF) as a list
     1---------0
     |         |-----> x
     2---------3
     :param rect:
     :return:
     """
-
     l2g: SE2value = SE2_from_xytheta((rect.center()[0], rect.center()[1], rect.orientation()))
     # vertices of the rectangle
     vertices = np.array([[rect.r_y(), -rect.r_y(), -rect.r_y(), rect.r_y()],
@@ -28,13 +29,25 @@ def get_vertices_from_rectangle(rect: pycrcc.RectOBB) -> List[List[float]]:
     return vertices.T.tolist()
 
 
+def get_vertices_as_tuple(rect: pycrcc.RectOBB) -> Tuple[Tuple[float, ...], ...]:
+    """ This gets the car vertices in the global reference frame (RF) as a tuple
+    1---------0
+    |         |-----> x
+    2---------3
+    :param rect:
+    :return:
+    """
+    vertices = get_vertices_as_list(rect)
+    return tuple([tuple(row) for row in vertices])
+
+
 def get_rectangle_mesh(footprint: pycrcc.RectOBB) -> Mapping[ImpactLocation, pycrcc.Triangle]:
     """
     This returns all the vertices of a rectangle in the global reference frame of the map (a bit useless for now)
     :return:
     """
 
-    vertices = get_vertices_from_rectangle(footprint)
+    vertices = get_vertices_as_list(footprint)
     rect_cx, rect_cy = footprint.center()
 
     impact_locations: Mapping[ImpactLocation, pycrcc.Triangle] = {
@@ -44,3 +57,20 @@ def get_rectangle_mesh(footprint: pycrcc.RectOBB) -> Mapping[ImpactLocation, pyc
         IMPACT_RIGHT: pycrcc.Triangle(*vertices[2], *vertices[3], *(rect_cx, rect_cy)),
     }
     return impact_locations
+
+
+def get_nearest_collision_points(vehicle_a: pycrcc.RectOBB, vehicle_b: pycrcc.RectOBB) -> np.ndarray:
+    center = Point(vehicle_a.center()[0], vehicle_a.center()[1])
+    vertices_b = get_vertices_as_tuple(vehicle_b)
+    vertices_b += (vertices_b[0],)
+    rect = Polygon(vertices_b)
+    nearest_pts = nearest_points(center, rect)
+    return np.array([[nearest_pts[0].x, nearest_pts[0].y], [nearest_pts[1].x, nearest_pts[1].y]])
+
+
+def get_normal_of_impact(vehicle_a: pycrcc.RectOBB, vehicle_b: pycrcc.RectOBB) -> np.ndarray:
+    nearest_pts = get_nearest_collision_points(vehicle_a, vehicle_b)
+    n = nearest_pts[1] - nearest_pts[0]  # Subtract nearest_point_b - center_of_a
+    n /= np.linalg.norm(n)               # Make it a unitary vector
+    return n
+

src/sim/models/vehicle.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 import numpy as np
 from commonroad_dc.pycrcc import RectOBB
 from frozendict import frozendict
-from geometry import SE2value, SE2_from_xytheta, SO2_from_angle, SO2
+from geometry import SE2value, SE2_from_xytheta, SO2_from_angle, SO2, SO2value
 from scipy.integrate import solve_ivp
 
 from sim.models.vehicle_structures import VehicleParameters, VehicleGeometry
@@ -207,10 +207,11 @@ class VehicleModel(SimModel[VehicleState, VehicleCommands]):
         return self.vg
 
     def get_velocity(self) -> np.ndarray:
+        # todo double check this!!!
         vx = self._state.vx
         dtheta = vx * math.tan(self._state.delta) / self.vg.length
         vy = dtheta * self.vg.lf
         v_l = np.array([[vx], [vy]])                    # Velocity in local RF
-        l2g: SO2 = SO2_from_angle(self._state.theta)  # Rotation matrix
+        l2g: SO2value = SO2_from_angle(self._state.theta)  # Rotation matrix
         v_g = l2g @ v_l                               # Velocity in global RF
-        return v_g
+        return v_g.T[0] # array([vx, vy])