6125f8ae89af1059acb6a6a50876f3ef197d14ae
Alessandro Zanardi <azanardi@ethz.ch>
2021-08-26 13:00:55 +0200

    combined coll (#79)

--- src/crash/reports.py
+++ src/crash/reports.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
+import networkx as nx
 from reprep import Report, MIME_PNG, MIME_GIF
 
+from crash import logger
+from crash.collisions_investigation import investigate_collision_report
 from crash.metrics_malliaris import compute_malliaris_one
 from crash.metrics_structures import CollMetricsReport
 from sim import SimulationLog, CollisionReport
-from crash import logger
 from sim.collision_visualisation import plot_collision
 from sim.simulator import SimContext
 from sim.simulator_animation import create_animation
@@ -29,7 +31,6 @@ def generete_report(sim_context: SimContext) -> Report:
     r = Report("Optimal crashing")
     if sim_context.sim_terminated is not True:
         raise RuntimeWarning("Generating a simulation report from a simulation that is not terminated")
-
     gif_viz = r.figure(cols=1)
     with gif_viz.data_file("Simulation", MIME_GIF) as fn:
         create_animation(file_path=fn,
@@ -43,9 +44,13 @@ def generete_report(sim_context: SimContext) -> Report:
 
 
 def get_collsion_reports(sim_context: SimContext) -> Report:
-    r = Report("Collison report")
+    r = Report("Collision report")
     logger.info(f"Generating collision images for {len(sim_context.collision_reports)} collisions")
-    for i, report in enumerate(sim_context.collision_reports):
+    accidents_report, coll_graph = investigate_collision_report(sim_context.collision_reports, True)
+    with r.plot(nid="CollisionGraph", mime=MIME_PNG) as pylab:
+        nx.draw(coll_graph, with_labels=True)
+    plt.close()
+    for i, report in enumerate(accidents_report):
         r.text(f"Collision-{i}", text=report.__str__())
         with r.data_file(f"Collision-{i}-viz", MIME_PNG) as f:
             plot_collision(report)
@@ -53,5 +58,4 @@ def get_collsion_reports(sim_context: SimContext) -> Report:
         plt.close()
         damage_metrics = compute_damage_metrics(coll_report=report, sim_log=sim_context.log)
         r.text(f"Collision-{i}-damages", text=damage_metrics.__str__())
-
     return r
