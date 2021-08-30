6c867617c4e5af7dcc8fbb24e5abf69be7e28396
Zelio Suter <suterz@ethz.ch>
2021-08-30 11:06:34 +0200

    Pure pursuit based on rear axle

--- src/dg_commons/controllers/pure_pursuit.py
+++ src/dg_commons/controllers/pure_pursuit.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 from math import sin, atan
 from typing import Optional, Tuple
+from sim.models.vehicle_structures import VehicleGeometry
 
 import numpy as np
 import scipy.optimize
-from geometry import SE2value, translation_angle_from_SE2
+from geometry import SE2value, translation_angle_from_SE2, SE2_from_rotation_translation
+from geometry.rotations import SO2_from_angle
 
 from dg_commons.geo import euclidean_between_SE2value
 from dg_commons.planning.lanes import DgLanelet
@@ -22,6 +24,7 @@ class PurePursuitParam:
     """Min initial progress to look for the next goal point"""
     max_extra_distance: float = 5
     """Max extra distance to look for the closest point on the ref path"""
+    vehicle_geometry: VehicleGeometry = VehicleGeometry.default_car()
 
 
 class PurePursuit:
@@ -38,6 +41,7 @@ class PurePursuit:
         """
         self.path: Optional[DgLanelet] = None
         self.pose: Optional[SE2value] = None
+        self.rear_pose: Optional[SE2value] = None
         self.along_path: Optional[float] = None
         self.speed: float = 0
         self.param: PurePursuitParam = params
@@ -52,6 +56,15 @@ class PurePursuit:
         assert isinstance(along_path, float)
         self.pose = pose
         self.along_path = along_path
+        self._update_rear_axle_pose()
+
+    def _update_rear_axle_pose(self):
+        tr, ang = translation_angle_from_SE2(self.pose)
+        rot = SO2_from_angle(ang)
+
+        delta_tr = np.dot(rot, np.array([-self.param.vehicle_geometry.lr, 0]).T)
+        tr += delta_tr
+        self.rear_pose = SE2_from_rotation_translation(rot, tr)
 
     def update_speed(self, speed: float):
         self.speed = speed
@@ -70,10 +83,10 @@ class PurePursuit:
             """
             beta = self.path.beta_from_along_lane(along_path)
             cp = self.path.center_point(beta)
-            dist = euclidean_between_SE2value(self.pose, cp)
+            dist = euclidean_between_SE2value(self.rear_pose, cp)
             return np.linalg.norm(dist - lookahead)
 
-        min_along_path = self.along_path + self.param.min_distance
+        min_along_path = self.along_path + self.param.min_distance - self.param.vehicle_geometry.lr
 
         bounds = [min_along_path,
                   min_along_path + lookahead + self.param.max_extra_distance]
@@ -88,13 +101,13 @@ class PurePursuit:
         # todo fixme this controller is not precise, as we use the cog rather than the base link
         if any([_ is None for _ in [self.pose, self.path]]):
             raise RuntimeError("Attempting to use PurePursuit before having set any observations or reference path")
-        p, theta = translation_angle_from_SE2(self.pose)
+        p, theta = translation_angle_from_SE2(self.rear_pose)
         _, goal_point = self.find_goal_point()
         p_goal, theta_goal = translation_angle_from_SE2(goal_point)
         alpha = np.arctan2(p_goal[1] - p[1], p_goal[0] - p[0]) - theta
         radius = self._get_lookahead() / (2 * sin(alpha))
         # here 3.5 is just an approximation of an average vehicle length
-        return atan(3.5 / radius)
+        return atan(self.param.vehicle_geometry.length / radius)
 
     def _get_lookahead(self) -> float:
         return float(np.clip(self.param.k_lookahead * self.speed,
