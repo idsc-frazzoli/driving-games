3ce988a6d1c21c92bfecfe7ed3522c2848111159
alezana <azanardi@ethz.ch>
2021-08-31 20:00:52 +0200

    minor

--- src/sim_tests/test_logs.py
+++ src/sim_tests/test_logs.py
@@ -1,15 +1,14 @@
 from numpy.testing import assert_raises
+from zuper_commons.types import ZValueError
 
 from games import PlayerName
 from sim import SimLog
 
 
-def test_bisect_simulationLog():
-    # todo test logs performance
+def test_illegal_simulationLog():
     log = SimLog()
 
     def tryillegal():
         log[0] = {PlayerName("P1"): 1, PlayerName("P1"): 2}
 
-    log[0] = {PlayerName("P1"): 1, PlayerName("P1"): 2}
-    #assert_raises(AssertionError, tryillegal)
+    assert_raises(ZValueError, tryillegal)
