82e03777ac33b40f8555425d5a8a29b87dbc8917
alezana <azanardi@ethz.ch>
2021-09-01 20:03:07 +0200

    steer controller

--- src/dg_commons/controllers/speed.py
+++ src/dg_commons/controllers/speed.py
@@ -15,12 +15,13 @@ from sim.models import extract_pose_from_state, kmh2ms
 
 @dataclass
 class SpeedControllerParam(PIDParam):
-    kP: float = 0.3
-    kI: float = 0.1
-    kD: float = 0
+    """Default values are tuned roughly for a default car model"""
+    kP: float = 1
+    kI: float = 0.01
+    kD: float = 0.1
     antiwindup: Tuple[float, float] = (-2, 2)
-    setpoint_minmax: Tuple[float, float] = (-kmh2ms(10), kmh2ms(100))
-    """In m/s"""
+    setpoint_minmax: Tuple[float, float] = (-kmh2ms(10), kmh2ms(150))
+    output_minmax: Tuple[float, float] = (-8, 5)  # acc minmax
 
 
 class SpeedController(PID):
