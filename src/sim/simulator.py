e4025a95dea669b2a6553b6a340b7368bf05586f
Zelio Suter <suterz@ethz.ch>
2021-09-16 16:28:25 +0200

    add noise to sim context

--- src/sim/simulator.py
+++ src/sim/simulator.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass, field
 from decimal import Decimal
 from itertools import combinations
-from typing import Mapping, Optional, List, Dict
+from typing import Mapping, Optional, List, Dict, Tuple
 
+import numpy as np
 from commonroad.scenario.scenario import Scenario
 
 from dg_commons.time import time_function
@@ -28,6 +29,7 @@ class SimContext:
     sim_terminated: bool = False
     collision_reports: List[CollisionReport] = field(default_factory=list)
     first_collision_ts: SimTime = SimTime("Infinity")
+    noise: Tuple[np.ndarray, np.ndarray] = (np.zeros(5), np.zeros((5, 5)))
 
     def __post_init__(self):
         assert self.models.keys() == self.players.keys()
@@ -63,7 +65,11 @@ class Simulator:
         self.last_observations.time = sim_context.time
         self.last_observations.players = {}
         for player_name, model in sim_context.models.items():
-            self.last_observations.players.update({player_name: model.get_state()})
+            state = model.get_state()
+            noise = np.zeros(state.get_n_states())
+            noise[:5] = np.random.multivariate_normal(sim_context.noise[0], sim_context.noise[1])
+            state = state.from_array(state.as_ndarray() + noise)
+            self.last_observations.players.update({player_name: state})
         logger.debug(f"Pre update function, sim time {sim_context.time}")
         logger.debug(f"Last observations:\n{self.last_observations}")
         return
