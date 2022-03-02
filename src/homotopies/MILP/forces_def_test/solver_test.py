839ff2344ba364f847f928714091212efbc0a02e
Yueshan Li <yuesli@student.ethz.ch>
2022-03-02 23:49:08 +0100

    add evaluation

--- src/homotopies/MILP/forces_def_test/solver_test.py
+++ src/homotopies/MILP/forces_def_test/solver_test.py
@@ -6,11 +6,11 @@ from dg_commons import PlayerName
 from homotopies.MILP.utils.prediction import predict, traj2lane, traj_from_commonroad
 from dg_commons.sim.scenarios import load_commonroad_scenario
 from homotopies.MILP.utils.intersects import find_intersects
-from homotopies.MILP.forces_def.sim import get_init_bin, update_sim, extract_plans
+from homotopies.MILP.forces_def.sim import get_init_bin, update_sim, extract_plans, check_status, update_evaluation
 from homotopies.MILP.forces_def.generate_solver import generate_solver
 from homotopies.MILP.forces_def.inequalities import get_ineq
 from homotopies.MILP.forces_def.report import *
-from homotopies.MILP.forces_def.parameters import params
+from homotopies.MILP.forces_def.parameters import params, inter_idx
 from homotopies.MILP.forces_def.forces_utils import ForcesException
 from reprep import Report
 
@@ -55,14 +55,14 @@ n_controlled = n_player
 n_inter = int(n_player * (n_player - 1) / 2)
 
 use_bin_init = True
-# generate_solver(n_player, n_controlled, n_inter, use_bin_init)
+generate_solver(n_player, n_controlled, n_inter, use_bin_init)
 
 import test_py
 
 problem = test_py.test_params
 
 # simulation
-sim_time = 45
+sim_time = 60
 if n_player == 2:
     x0 = np.array([0, state1.vx, 0, state2.vx])
 elif n_player == 3 and not p3_from_commonroad:
@@ -74,27 +74,42 @@ X = np.zeros((params.n_states * n_controlled, sim_time + 1))
 X_plans = np.zeros((params.n_states * n_controlled, params.N + 1, sim_time))
 dds_plans = np.zeros((params.n_cinputs * n_controlled, params.N, sim_time))
 bin_plans = np.zeros((params.n_binputs * n_inter, params.N, sim_time))
+solvetime = np.zeros(sim_time)
+result = {}
 X[:, 0] = x0
 
 h = {player1: {player2: 1, player3: 1}, player2: {player3: 1}}
+
 box_buffer = 1.5
 A, b = get_ineq(n_controlled, n_inter, trajs, intersects, h, box_buffer=box_buffer)
-solvetime = np.zeros(sim_time)
 
 # simulation
 for k in range(sim_time):
-    init_bin = get_init_bin(X[:, k], trajs, intersects, n_inter, box_buffer)
+    X_plans[:, 0, k] = X[:, k]
+    bin_init = get_init_bin(X[:, k], trajs, intersects, n_inter, box_buffer)
+    # check if all intersections are passed
+    task_status = check_status(bin_init, X[:, 0], X[:, k], trajs.keys())
+    result = update_evaluation(task_status, result, k, dds_plans)
+    if all(status for status in task_status.values()):  # if all tasks are finished, stop early
+        X_plans = X_plans[:, :, :k+1]
+        dds_plans = dds_plans[:, :, :k]
+        bin_plans = bin_plans[:, :, :k]
+        solvetime = solvetime[:k]
+        break
+
+    #  provide initial values
     if use_bin_init:
-        problem['minus_x0'] = -np.append(init_bin, X[:, k])
+        problem['minus_x0'] = -np.append(bin_init, X[:, k])
     else:
         problem['minus_x0'] = -X[:, k]
+
     for j in range(0, params.N):
         problem['ineq_A{:02d}'.format(j + 1)] = A
         problem['ineq_b{:02d}'.format(j + 1)] = b
     [solverout, exitflag, info] = test_py.test_solve(problem)
+
     if (exitflag == 1):
         X_plan, dds_plan, bin_plan = extract_plans(solverout, n_controlled, n_inter)
-        X_plans[:, 0, k] = X[:, k]
         X_plans[:, 1:, k] = X_plan
         dds_plans[:, :, k] = dds_plan
         bin_plans[:, :, k] = bin_plan
@@ -123,6 +138,7 @@ r.add_child(generate_report_s_traj(X_plans, trajs, intersects, buffer=box_buffer
 r.add_child(generate_report_input(dds_plans, n_controlled))
 r.add_child(generate_report_ds(X_plans, n_controlled))
 r.add_child(generate_report_solvetime(solvetime))
+r.add_child(generate_report_evaluation(result))
 # report_file = os.path.join("src/homotopies/MILP/forces_def_test", f"test_solver")
 report_file = "test_solver"
 r.to_html(report_file)
