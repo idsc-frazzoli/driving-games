100ba4d6f6879d1fd4577548717acea2e38c6603
Leon Zueger <leonzueger@bluewin.ch>
2022-03-10 14:00:29 +0100

    cleanup trajectory generation test

--- src/trajectory_games_tests/test_trajectory_generation.py
+++ src/trajectory_games_tests/test_trajectory_generation.py
@@ -1,7 +1,6 @@
 import os
 
 import matplotlib
-import matplotlib.pyplot as plt
 from geometry import translation_angle_from_SE2
 from decimal import Decimal as D
 
@@ -9,7 +8,7 @@ from dg_commons.maps import DgLanelet
 from dg_commons.planning import RefLaneGoal
 from dg_commons.sim.models.vehicle import VehicleState
 from dg_commons.sim.models.vehicle_structures import VehicleGeometry
-from dg_commons.sim.scenarios import DgScenario, load_commonroad_scenario
+from dg_commons.sim.scenarios import load_commonroad_scenario
 from dg_commons_dev.utils import get_project_root_dir
 
 from trajectory_games.trajectory_generator_dev import TransitionGenerator
@@ -17,41 +16,31 @@ from trajectory_games.structures import TrajectoryParams
 from trajectory_games.visualization_dev import TrajectoryGenerationVisualization
 
 
-
-
-
-
-
-
-
-if __name__ == "__main__":
+def test_trajectory_generation():
     SCENARIOS_DIR = os.path.join(get_project_root_dir(), "scenarios")
     scenario, _ = load_commonroad_scenario("DEU_Ffb-1_7_T-1", SCENARIOS_DIR)
-    #dgscenario = DgScenario(scenario)
     matplotlib.use("TkAgg")
-    #viz = TrajectoryGenerationVisualization(scenario=scenario)
-    #viz.plot(show_plot=True, draw_labels=True)
     lanelet_network = scenario.lanelet_network
 
-    points_from_first = 0
-    points_from_last = 0
+    points_from_first = 4
+    points_from_last = 4
 
     lane_11 = DgLanelet.from_commonroad_lanelet(lanelet_network.find_lanelet_by_id(49564))
     lane_12 = DgLanelet.from_commonroad_lanelet(lanelet_network.find_lanelet_by_id(49586))
     lane_13 = DgLanelet.from_commonroad_lanelet(lanelet_network.find_lanelet_by_id(49568))
 
-    dglane1_ctrl_points = lane_11.control_points[-points_from_last:-1] \
-                          + lane_12.control_points \
-                          + lane_13.control_points[1:points_from_first]
+    dglane_ctrl_points = lane_11.control_points[-points_from_last:-1] \
+                         + lane_12.control_points \
+                         + lane_13.control_points[1:points_from_first]
 
-    dglanelet = DgLanelet(dglane1_ctrl_points)
+    dglanelet = DgLanelet(dglane_ctrl_points)
 
     x_1_translation_angles = [translation_angle_from_SE2(dglanelet.center_point(beta)) for
                               beta in range(len(dglanelet.control_points))]
 
     ref_lane_goals = [RefLaneGoal(ref_lane=dglanelet, goal_progress=0.9)]
     init_point = x_1_translation_angles[1]
-    initial_state = VehicleState(x=init_point[0][0]+1, y=init_point[0][1], vx=0, theta=init_point[1], delta=0)
+    initial_state = VehicleState(x=init_point[0][0], y=init_point[0][1], vx=0, theta=init_point[1], delta=0)
     u_acc = frozenset([-1.0, 0.0, 1.0, 2.0, 3.0])
     u_dst = frozenset([_ * 0.2 for _ in u_acc])
     params = TrajectoryParams(
@@ -75,10 +64,11 @@ if __name__ == "__main__":
 
     viz = TrajectoryGenerationVisualization(scenario=scenario, trajectories=trajectories)
 
-
     dir_path = os.path.dirname(os.path.realpath(__file__))
     filename = os.path.join(dir_path, "out/trajectory_generation_test.png")
-    viz.plot(show_plot=True, action_color="red", filename=filename)
+    viz.plot(show_plot=False, action_color="red", filename=filename)
+    return 0
 
 
-    # save fig
+if __name__ == "__main__":
+    test_trajectory_generation()
