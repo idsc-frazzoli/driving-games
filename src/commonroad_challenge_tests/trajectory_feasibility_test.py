01c9f2e9eb5097cdf237ccc5492507e553c2d273
Leon Zueger <leonzueger@bluewin.ch>
2022-05-02 10:04:43 +0200

    trajectory feasibility test updated

--- src/commonroad_challenge_tests/trajectory_feasibility_test.py
+++ src/commonroad_challenge_tests/trajectory_feasibility_test.py
@@ -11,16 +11,18 @@ from commonroad_challenge.utils import traj_gen_params_from_cr, convert_to_cr_st
 from dg_commons.planning import RefLaneGoal, Trajectory
 from commonroad.scenario.trajectory import Trajectory
 from dg_commons.sim.models.vehicle import VehicleState
+from dg_commons.sim.models.vehicle_structures import VehicleGeometry
 from dg_commons.sim.scenarios import load_commonroad_scenario
 from dg_commons.sim.scenarios.agent_from_commonroad import dglane_from_position
 from dg_commons_dev.utils import get_project_root_dir
-from trajectory_games import TrajectoryGenerator, TrajectoryGenerationVisualization
+from trajectory_games import TrajectoryGenerator, TrajectoryGenerationVisualization, TrajectoryGenParams
 
 
 def check_feasibility(traj: Trajectory) -> bool:
     dt = traj.timestamps[1] - traj.timestamps[0]
     vehicle_type = VehicleType.FORD_ESCORT
     vehicle_dynamics = VehicleDynamics.KS(vehicle_type)
+
 
     vehicle_params = vehicle_parameters[vehicle_type]
 
@@ -29,8 +31,10 @@ def check_feasibility(traj: Trajectory) -> bool:
     values = traj.values
     for i, state in enumerate(values):
         current_state = convert_to_cr_state(state)
-        current_state.position[0] = current_state.position[0] - math.cos(current_state.orientation)*vehicle_params.l/2.0
-        current_state.position[1] = current_state.position[1] - math.sin(current_state.orientation)*vehicle_params.l/2.0
+        # current_state.position[0] = current_state.position[0] - math.cos(current_state.orientation)*vehicle_params.l/2.0
+        # current_state.position[1] = current_state.position[1] - math.sin(current_state.orientation)*vehicle_params.l/2.0
+        beta = math.atan2((vehicle_params.l/2) * math.tan(current_state.steering_angle), vehicle_params.l)
+        # current_state.velocity = current_state.velocity / math.cos(beta)**2
         current_state.time_step = i
         cr_traj_states.append(current_state)
 
@@ -51,20 +55,58 @@ def generate_trajectories(show_plot=False):
 
     ref_lane_goals = [RefLaneGoal(ref_lane=dglane, goal_progress=1000)]
 
-    initial_state = VehicleState(x=p[0], y=p[1], vx=0, theta=-0.02, delta=0)
+    initial_state = VehicleState(x=p[0], y=p[1], vx=10.0, theta=-0.02, delta=0)
 
     vehicle_type = VehicleType.FORD_ESCORT
     # vehicle_dynamics = VehicleDynamics.KS(vehicle_type)
 
     vehicle_params = vehicle_parameters[vehicle_type]
 
-    traj_gen_params = traj_gen_params_from_cr(cr_vehicle_params=vehicle_params)
+    traj_gen_params = traj_gen_params_from_cr(cr_vehicle_params=vehicle_params, is_ego=True)
+
+    traj_gen_params.u_dst = frozenset([-0.5, 0.5])
+    traj_gen_params.u_acc = frozenset([1.0, 2.0])
+    traj_gen_params.s_final = -1
+    traj_gen_params.n_factor = 1.2
+    traj_gen_params.solve = False
+
+
+
+
+
+    from decimal import Decimal as D
+    traj_gen_params.dt = D("1.0")
+
+    u_acc = frozenset([1.0, 3.0])  # todo: negative accelerations should also be possible
+    u_dst = frozenset([-0.1, 0.1])  # todo: not only "concave starts", also convex should be possible
+    # u_dst = frozenset([_ * 0.2 for _ in u_acc])
+
+    # todo: generate less curves but "longer"?
+    # traj_gen_params = TrajectoryGenParams(
+    #     solve=True,
+    #     s_final=-1,  # todo: add this to generator
+    #     max_gen=100,
+    #     dt=D("0.5"),  # keep at max 1 sec, increase k_maxgen in trajectrory_generator for having more generations
+    #     u_acc=u_acc,
+    #     u_dst=u_dst,
+    #     v_max=15.0,
+    #     v_min=0.0,
+    #     st_max=0.5,
+    #     dst_max=1.0,
+    #     dt_samp=D("0.2"),
+    #     dst_scale=False,
+    #     n_factor=1.0,  # todo: investigate what this does
+    #     vg=VehicleGeometry.default_car(),
+    # )
+
     generator = TrajectoryGenerator(params=traj_gen_params, ref_lane_goals=ref_lane_goals)
 
     traj_graph = generator.get_actions(state=initial_state, return_graphs=True)
     graph = list(traj_graph)[0]
     trajectories = graph.get_all_trajectories()
+    # trajectories = graph.get_all_transitions()
 
+    show_plot = True
     if show_plot:
         viz = TrajectoryGenerationVisualization(scenario=scenario, trajectories=trajectories)
 
@@ -78,6 +120,11 @@ def generate_trajectories(show_plot=False):
 
 if __name__ == "__main__":
     trajectories = generate_trajectories()
-
+    count = 0
     for traj in trajectories:
-        print(check_feasibility(traj))
+        feas = check_feasibility(traj)
+        if feas:
+            count = count+1
+        print(feas)
+
+    print(count)
