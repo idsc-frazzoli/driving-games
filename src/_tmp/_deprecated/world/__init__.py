374634b3c7b5adb8e4db892e7a2f9ecb1873c3d4
alezana <azanardi@ethz.ch>
2022-06-15 18:04:05 +0200

    clean deprecated

--- src/_tmp/_deprecated/world/__init__.py
+++
@@ -1,8 +0,0 @@
-from zuper_commons.logs import ZLogger
-
-logger = ZLogger(__name__)
-logger.warn("This module is becoming deprecated, consider using the commonroad scenarios")
-from .map_loading import *
-from .skeleton_graph import *
-from .tiles import *
-from .utils import *
